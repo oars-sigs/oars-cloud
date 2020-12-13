08b26b59d0d4f4e5888249f6c3103eb19e965752
hashwing <1144205233@qq.com>
2020-12-13 21:38:51 +0800

    add gzip in ui handle

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,11 +1,14 @@
 package server
 
 import (
+	"compress/gzip"
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/oars-sigs/oars-cloud/core"
@@ -17,9 +20,10 @@ func Start(mgr *core.APIManager) error {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
 	routers.NewV1(r, mgr)
-	r.GET("/", gin.WrapH(http.FileServer(ui.New())))
-	r.GET("/app.js", gin.WrapH(http.FileServer(ui.New())))
-	r.GET("/app.css", gin.WrapH(http.FileServer(ui.New())))
+	uiHandle := MakeGzipHandler(http.FileServer(ui.New()))
+	r.GET("/", gin.WrapH(uiHandle))
+	r.GET("/app.js", gin.WrapH(uiHandle))
+	r.GET("/app.css", gin.WrapH(uiHandle))
 
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", mgr.Cfg.Server.Port),
@@ -49,4 +53,32 @@ func Start(mgr *core.APIManager) error {
 
 	err := s.ListenAndServe()
 	return err
+}
+
+type gzipResponseWriter struct {
+	io.Writer
+	http.ResponseWriter
+}
+
+// Use the Writer part of gzipResponseWriter to write the output.
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+// MakeGzipHandler adds support for gzip compression for given handler
+func MakeGzipHandler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check if the client can accept the gzip encoding.
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			handler.ServeHTTP(w, r)
+			return
+		}
+
+		// Set the HTTP header indicating encoding.
+		w.Header().Set("Content-Encoding", "gzip")
+		gz := gzip.NewWriter(w)
+		defer gz.Close()
+		gzw := gzipResponseWriter{Writer: gz, ResponseWriter: w}
+		handler.ServeHTTP(gzw, r)
+	})
 }
